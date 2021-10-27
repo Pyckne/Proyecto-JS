c4460b621ce077140579e97f97771f429fb42ee2
Pyckne <lisandro9604@gmail.com>
2021-10-27 20:34:58 -0300

    Limpiando Header y Footer e insertandolos por js

---
+++ src/dom/footer.js
@@ -0,0 +1,16 @@
+$('#footer').append(`<div class="container d-flex justify-content-center">
+<div class="row">
+    <div class="col">
+        <h6 class="h3">Moon Shop</h6>
+        <p>Comodoro Rivadavia, Argentina.</p>
+    </div>
+    <div class="col">
+        <h6 class="h3">Contactanos</h6>
+        <div>
+            <a href="mailto:lisandro9604@gmail.com" id="contacto"><img src="./src/assets/mail.png" alt="Mail" id="icon"></a>
+            <a href="https://www.facebook.com/" id="contacto"><img src="./src/assets/facebook.png" alt="Facebook" id="icon"></a>
+            <a href="https://www.instagram.com/" id="contacto"><img src="./src/assets/Instagram.png" alt="Instagram" id="icon"></a>
+        </div>
+    </div>
+</div>
+</div>`)
---
+++ src/dom/header.js
@@ -0,0 +1,11 @@
+$('#header').append(`<ul class="nav nav-pills py-3 container" id="pills-tab" role="tablist">
+<li class="nav-item" role="presentation">
+    <a class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="a" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+</li>
+<li class="nav-item" role="presentation">
+    <a class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="a" role="tab" aria-controls="pills-profile" aria-selected="false">Carrito</a>
+</li>
+<li class="nav-item" role="presentation">
+    <a class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="a" role="tab" aria-controls="pills-contact" aria-selected="false">Contacto</a>
+</li>
+</ul>`)
